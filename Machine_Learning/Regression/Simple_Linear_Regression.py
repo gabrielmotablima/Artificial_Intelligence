547f39e7203d6b20749178f52a8a6303e73eef4d
Gabriel Mota Bromonschenkel Lima <31813682+GabrielMotaBLima@users.noreply.github.com>
2019-06-17 01:35:40 -0300

    Update Simple_Linear_Regression.py

--- Machine_Learning/Regression/Simple_Linear_Regression.py
+++ Machine_Learning/Regression/Simple_Linear_Regression.py
@@ -12,12 +12,12 @@ import pandas as pd
 # Importing the dataset
 # For .csv data in the same code's folder, otherwise you will have to specify the file address.
 # The dataset have to be in format: | ID COLUMN | FEATURE COLUMNS | LABEL COLUMN |
-# Just set YOUR_DATA.csv in read_csv pandas function, INDEPENDENT_VARIABLES_COLUMNS in iloc,
-# DEPENDENT_VARIABLE_COLUMN iloc() and see what happens
 dataset = pd.read_csv('YOUR_DATA.csv')
 print(dataset)
 
 # Visualizing the Independent and Dependent Variables
+# Just set YOUR_DATA.csv in read_csv pandas function, INDEPENDENT_VARIABLES_COLUMNS in iloc,
+# DEPENDENT_VARIABLE_COLUMN iloc() and see what happens
 X = dataset.iloc[:, [INDEPENDENT_VARIABLES_COLUMNS]].values
 y = dataset.iloc[:, DEPENDENT_VARIABLE_COLUMN].values
 
