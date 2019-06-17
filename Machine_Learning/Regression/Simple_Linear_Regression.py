45baefc295236fb59b9d0943e2d9afc7d98544ed
Gabriel Mota Bromonschenkel Lima <31813682+GabrielMotaBLima@users.noreply.github.com>
2019-06-17 01:24:46 -0300

    Update Simple_Linear_Regression.py

--- Machine_Learning/Regression/Simple_Linear_Regression.py
+++ Machine_Learning/Regression/Simple_Linear_Regression.py
@@ -5,41 +5,39 @@
 ###################################################################################################
 
 # Importing the Libraries
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-
 # For .csv data in the same code's folder, otherwise you will have to specify the file address.
 # The dataset have to be in format: | ID COLUMN | FEATURE COLUMNS | LABEL COLUMN |
 # Just set YOUR_DATA.csv in read_csv pandas function, INDEPENDENT_VARIABLES_COLUMNS in iloc,
 # DEPENDENT_VARIABLE_COLUMN iloc() and see what happens
-
 dataset = pd.read_csv('YOUR_DATA.csv')
+print(dataset)
+
+# Visualizing the Independent and Dependent Variables
 X = dataset.iloc[:, [INDEPENDENT_VARIABLES_COLUMNS]].values
 y = dataset.iloc[:, DEPENDENT_VARIABLE_COLUMN].values
 
 # Splitting the dataset into the Training set and Test set
-
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 # Fitting Linear Regression to the Training set
-
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
-
 y_pred = regressor.predict(X_test)
 
 # Try printing y_pred, y_test and observe the results
+print(y_pred)
+print(y_test)
 
 # Visualizing the Training set results
-
 plt.scatter(X_train, y_train, color = 'red')
 plt.scatter(X_test, y_pred, color = 'green')
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
