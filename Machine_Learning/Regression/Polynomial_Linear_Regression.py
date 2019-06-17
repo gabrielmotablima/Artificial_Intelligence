c347e034218517ce87904d5e791ea669cfc67424
Gabriel Mota Bromonschenkel Lima <31813682+GabrielMotaBLima@users.noreply.github.com>
2019-06-17 01:35:05 -0300

    Update Polynomial_Linear_Regression.py

--- Machine_Learning/Regression/Polynomial_Linear_Regression.py
+++ Machine_Learning/Regression/Polynomial_Linear_Regression.py
@@ -10,18 +10,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-
 # For .csv data in the same code's folder, otherwise you will have to specify the file address.
 # The dataset have to be in format: | ID COLUMN | FEATURE COLUMNS | LABEL COLUMN |
-# Just set YOUR_DATA.csv in read_csv pandas function, INDEPENDENT_VARIABLES_COLUMNS in iloc,
-# DEPENDENT_VARIABLE_COLUMN iloc() and see what happens
-
 dataset = pd.read_csv('YOUR_DATA.csv')
 print(dataset)
 
 # Visualizing the Independent and Dependent Variables
-X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2].values
+# Just set YOUR_DATA.csv in read_csv pandas function, INDEPENDENT_VARIABLES_COLUMNS in iloc,
+# DEPENDENT_VARIABLE_COLUMN iloc() and see what happens
+X = dataset.iloc[:, [INDEPENDENT_VARIABLES_COLUMNS]].values
+y = dataset.iloc[:, DEPENDENT_VARIABLE_COLUMN].values
 
 # Fitting Linear Regression Model of the Dataset
 from sklearn.linear_model import LinearRegression
@@ -39,17 +37,17 @@ lin_reg_2.fit(X_poly, y)
 # Visualising the Linear Regression results
 plt.scatter(X, y, color = 'red')
 plt.plot(X, lin_reg.predict(X), color = 'blue')
-plt.title('Truth or Bluff (Linear Regression)')
-plt.xlabel('Position Level')
-plt.ylabel('Salary')
+plt.title('Linear Regression')
+plt.xlabel('DEPENDENT_VARIABLES_COLUMNS (X_VARIABLES)')
+plt.ylabel('INDEPENDENT_VARIABLES_COLUMNS (Y_VARIABLES)')
 plt.show()
 
 # Visualising the Polynomial Regression results
 plt.scatter(X, y, color = 'red')
 plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
-plt.title('Truth or Bluff (Polynomial Regression)')
-plt.xlabel('Position Level')
-plt.ylabel('Salary')
+plt.title('Polynomial Regression')
+plt.xlabel('DEPENDENT_VARIABLES_COLUMNS (X_VARIABLES)')
+plt.ylabel('INDEPENDENT_VARIABLES_COLUMNS (Y_VARIABLES)')
 plt.show()
 
 # Visualising the Polynomial Regression results (for higher resolution and smoother curve)
@@ -57,9 +55,9 @@ X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape(len(X_grid), 1)
 plt.scatter(X, y, color = 'red' )
 plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
-plt.title('Truth of Bluff (Polynomial Regression)')
-plt.xlabel('Position Level')
-plt.ylabel('Salary')
+plt.title('Polynomial Regression')
+plt.xlabel('DEPENDENT_VARIABLES_COLUMNS (X_VARIABLES)')
+plt.ylabel('INDEPENDENT_VARIABLES_COLUMNS (Y_VARIABLES)')
 plt.show()
 
 # Predicting a new result with Linear Regression
