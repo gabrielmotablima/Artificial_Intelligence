fb2172899b5c8b20fccc6df07e5e4c047adb5043
Gabriel Mota Bromonschenkel Lima <gabriel.mota.bromonschenkel.lima@hotmail.com>
2019-06-27 19:58:20 -0300

    Update K-NN.py

--- Machine_Learning/Classification/K-NN.py
+++ Machine_Learning/Classification/K-NN.py
@@ -1,9 +1,19 @@
+    
+###################################################################################################
+###################################### K - Nearest Neighbors ######################################
+###################################################################################################
+##################################### Run on Jupyter Notebook #####################################
+###################################################################################################
+
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+# Importing the dataset, you can just substitute YOUR_DATE for your file's name,
+# choose the interval of independent and dependent variables in your CSV file
+# And finally run the code, change it on INDEPENDENT_VARIABLES_COLUMNS and
+# DEPENDENT_VARIABLES_COLUMN.
 dataset = pd.read_csv('YOUR_DATA.csv')
 X = dataset.iloc[:, [INDEPENDENT_VARIABLES_COLUMNS]].values
 y = dataset.iloc[:, DEPENDENT_VARIABLES_COLUMN].values
@@ -35,14 +45,14 @@ cm
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+					 np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+			 alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
+	plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+				c = ListedColormap(('red', 'green'))(i), label = j)
 plt.title('K-NN (Training set)')
 plt.xlabel('X_VARIABLES')
 plt.ylabel('Y_VARIABLES')
