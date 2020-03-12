7a3e75f61a7b467000d1c2b54f15df3111189687
Gabriel Mota Bromonschenkel Lima <gabriel.mota.bromonschenkel.lima@hotmail.com>
2020-03-11 22:00:16 -0300

    Update Simple_Linear_Regression.py

--- Machine_Learning/Regression/Simple_Linear_Regression.py
+++ Machine_Learning/Regression/Simple_Linear_Regression.py
@@ -12,14 +12,14 @@ import pandas as pd
 # Importing the dataset
 # For .csv data in the same code's folder, otherwise you will have to specify the file address.
 # The dataset have to be in format: | ID COLUMN | FEATURE COLUMNS | LABEL COLUMN |
-dataset = pd.read_csv('YOUR_DATA.csv')
+dataset = pd.read_csv('<YOUR_DATA>.csv')
 print(dataset)
 
 # Visualizing the Independent and Dependent Variables
 # Just set YOUR_DATA.csv in read_csv pandas function, INDEPENDENT_VARIABLES_COLUMNS in iloc(),
 # DEPENDENT_VARIABLE_COLUMN in iloc() and see what happens
-X = dataset.iloc[:, [INDEPENDENT_VARIABLES_COLUMNS]].values
-y = dataset.iloc[:, DEPENDENT_VARIABLE_COLUMN].values
+X = dataset.iloc[:, [<INDEPENDENT_VARIABLES_COLUMNS>]].values
+y = dataset.iloc[:, <DEPENDENT_VARIABLE_COLUMN>].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
